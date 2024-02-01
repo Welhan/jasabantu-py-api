a3c1253f7183819451917b09c42cd9fb502e8ada
welhan <78246470+Welhan@users.noreply.github.com>
2024-02-01 21:37:45 +0700

    datetime sesuai zona lokasi (pip install pytz)

--- models/otpModels.py
+++ models/otpModels.py
@@ -1,12 +1,14 @@
 from database import mysql
 from datetime import datetime
+import pytz
 
 class Otp:
     OTP_REQUEST_TABLE = "otp_request"
+    lokasi = 'Asia/Jakarta'
+    zona_waktu = pytz.timezone(lokasi)
     def create_otp(self, phone, otp):
         cur = mysql.connection.cursor()
-        query = "INSERT INTO {} (Phone, Otp) VALUES (%s, %s)".format(self.OTP_REQUEST_TABLE)
-        cur.execute(query, (phone, otp))
-        # cur.execute("INSERT INTO otp_request (Phone, Otp) VALUES (%s, %s)", (phone, otp))
+        query = "INSERT INTO {} (Phone, Otp,CreatedDate) VALUES (%s, %s, %s)".format(self.OTP_REQUEST_TABLE)
+        cur.execute(query, (phone, otp,datetime.now(self.zona_waktu)))
         mysql.connection.commit()
         cur.close()
