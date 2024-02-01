60ce1cd0757b341c257b62d77a23dac0381a9b94
welhan <78246470+Welhan@users.noreply.github.com>
2024-02-01 19:19:43 +0700

    fixing INSERT DB with Constant nama table

--- models/otpModels.py
+++ models/otpModels.py
@@ -5,7 +5,8 @@ class Otp:
     OTP_REQUEST_TABLE = "otp_request"
     def create_otp(self, phone, otp):
         cur = mysql.connection.cursor()
-        # cur.execute("INSERT INTO {} (Phone, Otp) VALUES (%s, %s)", (phone, otp)).format(self.OTP_REQUEST_TABLE)
-        cur.execute("INSERT INTO otp_request (Phone, Otp) VALUES (%s, %s)", (phone, otp))
+        query = "INSERT INTO {} (Phone, Otp) VALUES (%s, %s)".format(self.OTP_REQUEST_TABLE)
+        cur.execute(query, (phone, otp))
+        # cur.execute("INSERT INTO otp_request (Phone, Otp) VALUES (%s, %s)", (phone, otp))
         mysql.connection.commit()
         cur.close()
