33cc9fa21cf5911c4117d76c45ddf828dbfd2850
welhan <78246470+Welhan@users.noreply.github.com>
2024-02-05 16:57:20 +0700

    merapikan helpers

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -3,26 +3,18 @@ from models.otpModels import Otp
 import random
 
 
-
 user_model = User()
 otp_model = Otp()
 
 def generate_otp():
     return str(random.randint(1000,9999))
     
-
-
 def checkOtp(otp):
     checkOtp = otp_model.checkOtp(otp)
     if checkOtp is None:
         return False
     else:
         return True
-
-
-
-
-
 
 def checkPin(pin):
     checkPin = user_model.checkPin(pin)
