00877edea61cb2ff63c2ab177a650ac6fc2cf34c
welhan <78246470+Welhan@users.noreply.github.com>
2024-02-09 22:58:39 +0700

    merapikan file config.py

--- config/config.py
+++ config/config.py
@@ -1,10 +1,3 @@
-# # Database configuration
-# MYSQL_HOST = 'localhost'
-# MYSQL_USER = 'root'  # Ganti dengan username MySQL Anda
-# MYSQL_PASSWORD = ''  # Ganti dengan password MySQL Anda
-# MYSQL_DB = 'jasabantu'  # Ganti dengan nama database Anda
-
-
 import mysql.connector
 
 def create_connection():
